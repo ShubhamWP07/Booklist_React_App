9621728581dba118751ace965172f5bf0251a2ae
ShubhamWP07 <shubham1900384@gmail.com>
2023-05-13 16:16:29 +0530

    editing book

--- src/Components/BookEdit.js
+++ src/Components/BookEdit.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
-const BookEdit = () => {
-  return <div>BookEdit</div>;
+const BookEdit = ({ book }) => {
+  const [title, setTitle] = useState(book.title);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="book-edit">
+        <label>Title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button is-primary">Save</button>
+      </form>
+    </div>
+  );
 };
 
 export default BookEdit;
